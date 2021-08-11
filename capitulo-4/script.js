92adf343958a0bb78779f3b14f8dc7f0594b4e43
jcanaviri <jcanaviri20@gmail.com>
2021-08-10 22:45:07 -0400

    Using querySelectorAll

--- capitulo-4/script.js
+++ capitulo-4/script.js
@@ -3,7 +3,10 @@ function showAlert() {
 }
 
 function makeClick() {
-  document.querySelector('p').onclick = showAlert;
+  let list = document.querySelectorAll('p');
+  list.forEach(item => {
+    item.onclick = showAlert;
+  });
 }
 
 window.onload = makeClick;
