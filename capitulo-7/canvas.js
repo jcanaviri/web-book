423e85b7bf5212f145410c8c6f1af455846571ed
jcanaviri <jcanaviri20@gmail.com>
2021-08-20 12:38:30 -0400

    Making a triangle

--- capitulo-7/canvas.js
+++ capitulo-7/canvas.js
@@ -2,13 +2,12 @@ function iniciar() {
   let elemento = document.getElementById('lienzo');
   let lienzo = elemento.getContext('2d');
 
-  let gradiente = lienzo.createRadialGradient(0, 0, 30, 0, 0, 300);
-  gradiente.addColorStop(0.5, '#0000ff');
-  gradiente.addColorStop(1, '#000000');
-  lienzo.fillStyle = gradiente;
-
-  lienzo.fillRect(10, 10, 100, 100);
-  lienzo.fillRect(150, 10, 200, 100);
+  lienzo.beginPath();
+  lienzo.moveTo(100, 100);
+  lienzo.lineTo(200, 200);
+  lienzo.lineTo(100, 200);
+  lienzo.fill();
+  lienzo.stroke();
 }
 
 window.addEventListener('load', iniciar, false);
