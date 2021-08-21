0af66609c1bda736f31e3c6f81976b0f8a22129f
jcanaviri <jcanaviri20@gmail.com>
2021-08-20 22:55:41 -0400

    Using setDragImage

--- capitulo-8/dragdrop.js
+++ capitulo-8/dragdrop.js
@@ -1,13 +1,14 @@
 'use strict';
 
 let imagenes = document.querySelectorAll('#cajaimagenes > img');
-let soltar = document.getElementById('cajasoltar');
+let soltar = document.getElementById('lienzo');
+let ctx = soltar.getContext('2d');
 
 function iniciar() {
   imagenes.forEach((img) => {
     img.addEventListener('dragstart', arrastrado, false);
+    img.addEventListener('dragend', finalizado, false);
   });
-
 
   soltar.addEventListener('dragenter', function(e) {e.preventDefault();}, false);
   soltar.addEventListener('dragover', function(e) {e.preventDefault();}, false);
@@ -15,23 +16,30 @@ function iniciar() {
   soltar.addEventListener('drop', soltado, false);
 }
 
+function finalizado(e) {
+  let elemento = e.target;
+  elemento.style.visibility = 'hidden';
+}
+
 function arrastrado(e) {
   e.dataTransfer.effectAllowed = 'move';
   let elemento = e.target;
-  let codigo = elemento.getAttribute('id');
-  e.dataTransfer.setData('text', codigo);
+  e.dataTransfer.setData('text', elemento.getAttribute('id'));
+  
+  let img = new Image();
+  img.src = e.target.src;
+  e.dataTransfer.setDragImage(img, 0, 0);
 }
 
 function soltado(e) {
   e.preventDefault();
   let id = e.dataTransfer.getData('text');
-  if (id != 'img4') {
-    let src = document.getElementById(id).src;
-    soltar.innerHTML = `<img src="${src}">`;
-  } else {
-    soltar.innerHTML = 'La imagen no es valida';
-  }
+  let elemento = document.getElementById(id);
 
+  let posx = e.pageX;
+  let posy = e.pageY;
+
+  ctx.drawImage(elemento, posx, posy);
 }
 
 window.addEventListener('load', iniciar, false);
